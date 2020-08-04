1cf79315eb44df5cd35734abe28ad525723788f3
davide-amabile <davideamabile81@gmail.com>
2020-08-04 14:41:35 +0200

    biglietto prezzo intero

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+// chidere distanza ed eta
+var distance = parseInt(prompt("Immetere distanza in km?"));
 
+var age =  parseInt(prompt("Immettere la sua età?"));
+
+// calcolo per il prezzo biglietto intero
+var calcDist = (distance * 0.21);
+
+// condizioni per lo sconto
